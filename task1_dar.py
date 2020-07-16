60d19a511c7e81f6b24c658a8604828e7233c037
Hamza <hamza_dar1@live.com>
2020-07-16 17:18:23 +0500

    Python tasks 1-3

---
+++ task1_dar.py
@@ -0,0 +1,8 @@
+
+def sum(x,y):
+    print(x+y)
+
+a= float(input("Enter first number: "))
+b= float(input("Enter Second number: "))
+
+sum(a,b)
---
+++ task3_dar.py
@@ -0,0 +1,9 @@
+squares= {}
+
+n= int(input("Enter your number: "))
+
+for i in range (1, n+1):
+	squares[i]= i*i
+	
+print(squares)
+	
